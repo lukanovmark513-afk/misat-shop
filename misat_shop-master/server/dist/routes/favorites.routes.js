f34f0320d5b70412fe4177c85381f45ff94d82da
lukanovmark513-afk <lukanovmark513@gmail.com>
2026-06-14 13:51:14 +0300

    chore: update .gitignore - exclude node_modules, .idea, __pycache__

--- misat_shop-master/server/dist/routes/favorites.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const favoritesController_1 = require("../controllers/favoritesController");
-const auth_1 = require("../middleware/auth");
-const router = (0, express_1.Router)();
-router.get('/', auth_1.authMiddleware, favoritesController_1.getFavorites);
-router.post('/:productId', auth_1.authMiddleware, favoritesController_1.toggleFavorite);
-exports.default = router;
